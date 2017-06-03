0bcf20dc5842e1a175a0c54fa987ff34dd4d52ca
Gerardo Saca <gsacavdm@hotmail.com>
2017-06-02 22:25:56 -0700

    Remove AppInsights

--- B2C-WebApi/Program.cs
+++ B2C-WebApi/Program.cs
@@ -17,7 +17,6 @@ namespace B2CWebApi
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
